ff148eb8581684a9cd61eabdfe5a15b7ea6b6d89
simpelito <simonciesluk@gmail.com>
2017-05-21 00:30:35 +0200

    Improved physics

--- game.js
+++ game.js
@@ -13,8 +13,10 @@ window.onload = function() {
 }
 
 function preload(){
-    game.load.image('red_ship', 'assets/images/red_ship_1.png');
+    game.load.image('red_ship_1', 'assets/images/red_ship_1.png');
+    game.load.image('green_ship_2', 'assets/images/green_ship_2.png');
     game.load.image('red_spaceball', 'assets/images/red_spaceball.png');
+    game.load.physics('physicsData', './assets/polygons/polygons.json');
 }
 
 var ships = [];
@@ -22,19 +24,20 @@ var spaceball;
 
 function create(){
     game.stage.backgroundColor = "#404040";
-    game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    spaceball = game.add.sprite(576, 236, 'red_spaceball');
-    game.physics.enable(spaceball, Phaser.Physics.ARCADE);
-    spaceball.body.collideWorldBounds = true;
-    spaceball.body.bounce.set(1);
+    //	Enable p2 physics
+  	game.physics.startSystem(Phaser.Physics.P2JS);
+
+    //  Make things a bit more bouncey
+    game.physics.p2.defaultRestitution = 0.8;
 
     ships[0] = new ship(
         Phaser.Keyboard.A,
         Phaser.Keyboard.D,
         Phaser.Keyboard.W,
         Phaser.Keyboard.S,
-        200, 200
+        200, 200,
+        'red_ship_1'
     );
 
     ships[1] = new ship(
@@ -42,20 +45,44 @@ function create(){
         Phaser.Keyboard.RIGHT,
         Phaser.Keyboard.UP,
         Phaser.Keyboard.DOWN,
-        400, 400
+        400, 400,
+        'green_ship_2'
     );
+
+    spaceball = game.add.sprite(576, 236, 'red_spaceball');
+  	game.physics.p2.enable(spaceball, false);
+  	//	Clear the shapes and load the 'contra2' polygon from the physicsData JSON file in the cache
+  	spaceball.body.clearShapes();
+  	spaceball.body.loadPolygon('physicsData', 'red_spaceball');
+    spaceball.body.mass = 0.2;
+
+    var ballMaterial = game.physics.p2.createMaterial('ballMaterial', spaceball.body);
+    var worldMaterial = game.physics.p2.createMaterial('worldMaterial');
+
+    //  4 trues = the 4 faces of the world in left, right, top, bottom order
+    game.physics.p2.setWorldMaterial(worldMaterial, true, true, true, true);
+
+    //  Here is the contact material. It's a combination of 2 materials, so whenever shapes with
+    //  those 2 materials collide it uses the following settings.
+    //  A single material can be used by as many different sprites as you like.
+    var contactMaterial = game.physics.p2.createContactMaterial(ballMaterial, worldMaterial);
+
+    contactMaterial.friction = 0.3;     // Friction to use in the contact of these two materials.
+    contactMaterial.restitution = 1.0;  // Restitution (i.e. how bouncy it is!) to use in the contact of these two materials.
+    contactMaterial.stiffness = 1e7;    // Stiffness of the resulting ContactEquation that this ContactMaterial generate.
+    contactMaterial.relaxation = 3;     // Relaxation of the resulting ContactEquation that this ContactMaterial generate.
+    contactMaterial.frictionStiffness = 1e7;    // Stiffness of the resulting FrictionEquation that this ContactMaterial generate.
+    contactMaterial.frictionRelaxation = 3;     // Relaxation of the resulting FrictionEquation that this ContactMaterial generate.
+    contactMaterial.surfaceVelocity = 0;        // Will add surface velocity to this material. If bodyA rests on top if bodyB, and the surface velocity is positive, bodyA will slide to the right.
+
 }
 
-function ship(left, right, up, down, startPosX, startPosY){
-    this.sprite = game.add.sprite(startPosX, startPosY, 'red_ship');
-    this.sprite.name = 'ship';
-    this.sprite.anchor.set(0.5);
+function ship(left, right, up, down, startPosX, startPosY, ship_sprite){
+    this.sprite = game.add.sprite(startPosX, startPosY, ship_sprite);
 
-    game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
-
-    this.sprite.body.collideWorldBounds = true;
-    this.sprite.body.bounce.set(1);
-    this.sprite.body.allowRotation = true;
+    game.physics.p2.enable(this.sprite, false);
+    this.sprite.body.clearShapes();
+  	this.sprite.body.loadPolygon('physicsData', ship_sprite);
 
     this.leftKey = left;
     this.rightKey = right;
@@ -64,34 +91,33 @@ function ship(left, right, up, down, startPosX, startPosY){
 }
 
 function update(){
-    game.physics.arcade.collide(ships[0].sprite, ships[1].sprite);
-    game.physics.arcade.collide(spaceball, ships[1].sprite);
-    game.physics.arcade.collide(spaceball, ships[0].sprite);
     for(i = 0; i < ships.length; i ++){
         moveShip(ships[i]);
     }
 }
 
 function moveShip(ship){
-    ship.sprite.body.velocity.x = 0;
-    ship.sprite.body.velocity.y = 0;
-    ship.sprite.body.angularVelocity = 0;
-
     if (game.input.keyboard.isDown(ship.leftKey))
     {
-        ship.sprite.body.angularVelocity = -200;
+        //ship.sprite.body.rotateLeft(200);
+        ship.sprite.body.angularVelocity = -3;
     }
     else if (game.input.keyboard.isDown(ship.rightKey))
     {
-        ship.sprite.body.angularVelocity = 200;
+        //ship.sprite.body.rotateRight(200);
+        ship.sprite.body.angularVelocity = 3;
     }
+    else {ship.sprite.body.setZeroRotation();}
 
     if (game.input.keyboard.isDown(ship.upKey))
     {
-        ship.sprite.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(ship.sprite.angle, 400));
+        //ship.sprite.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(ship.sprite.angle, 400));
+        ship.sprite.body.thrust(1000);
     }
     else if (game.input.keyboard.isDown(ship.downKey))
     {
-        ship.sprite.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(ship.sprite.angle, -400));
+        //ship.sprite.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(ship.sprite.angle, -400));
+        ship.sprite.body.reverse(800);
     }
+    else {ship.sprite.body.damping = 0.8;}
 }
